86618dd0833bb603edd4386cf932ef933811bc15
Pavla Taterová <taterova.pavla@gmail.com>
2024-02-18 12:07:02 +0100

    adding function displayLibrary + button Add book

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const myLibrary = [];
+const myLibrary = [
+  {
+    title: "The Hobbit",
+    author: "Tolkien",
+    pages: "295",
+    read: "not yeat",
+  },
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    pages: "296",
+    read: "almost",
+  },
+  {
+    title: "Konec stárnutí",
+    author: "David Sinclair",
+    pages: "543",
+    read: "almost",
+  },
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -19,6 +38,35 @@ const theHobbit = new Book(
 
 console.log(theHobbit.info());
 
+const dialogElement = document.querySelector("dialog");
+const showButtonElement = document.getElementById("show-dialog");
+const submitButtonElement = document.getElementById("submit-button");
+const cancelButtonElement = document.getElementById("cancel-button");
+
+showButtonElement.addEventListener("click", () => {
+  dialogElement.showModal();
+});
+
+submitButtonElement.addEventListener("click", () => {
+  dialogElement.close();
+});
+
+cancelButtonElement.addEventListener("click", (e) => {
+  e.preventDefault();
+  dialogElement.close();
+});
+
+const displayLibrary = () => {
+  let bookListElement = document.getElementById("book-list");
+  bookListElement.innerHTML = "";
+
+  myLibrary.forEach((book) => {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = `Title: ${book.title} Author: ${book.author} Pages: ${book.pages} Read: ${book.read}`;
+    bookListElement.appendChild(paragraph);
+  });
+};
+
 function addBookToLibrary() {
   let titleInput = document.getElementById("title");
   let authorInput = document.getElementById("author");
@@ -26,19 +74,30 @@ function addBookToLibrary() {
   let readInput = document.getElementById("read");
   let bookForm = document.getElementById("form");
 
-  console.log(bookForm);
-
   bookForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    console.log("Form submitted");
     const newBook = new Book(
       titleInput.value,
       authorInput.value,
       pagesInput.value,
       readInput.value
     );
-
     myLibrary.push(newBook);
     console.log(myLibrary);
+
+    // Update the library display
+    displayLibrary();
+
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readInput.value = "";
   });
 }
+
+// Initial display of the library
+displayLibrary();
+
+// Call addBookToLibrary after initial display
 addBookToLibrary();
