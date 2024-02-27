f2ac1b5fe222de56d2f58082876f01c9262d90bb
Pavla Taterová <taterova.pavla@gmail.com>
2024-02-27 09:42:44 +0100

    adding removeBookButton, toggleReadButton + code cleaning(function createCard, function renderCard)

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const myLibrary = [
     title: "The Hobbit",
     author: "Tolkien",
     pages: "295",
-    read: "not yeat",
+    read: false,
   },
   {
     title: "Atomic Habits",
     author: "James Clear",
     pages: "296",
-    read: "almost",
+    read: true,
   },
   {
     title: "Konec stárnutí",
     author: "David Sinclair",
     pages: "543",
-    read: "almost",
+    read: true,
   },
 ];
 
@@ -29,12 +29,7 @@ function Book(title, author, pages, read) {
   };
 }
 
-const theHobbit = new Book(
-  "The Hobbit",
-  "J.R.R. Tolkien",
-  "295 pages",
-  "not read yet"
-);
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", false);
 
 console.log(theHobbit.info());
 
@@ -56,24 +51,77 @@ cancelButtonElement.addEventListener("click", (e) => {
   dialogElement.close();
 });
 
-const displayLibrary = () => {
-  let bookListElement = document.getElementById("book-list");
+function createCard(bookListElement) {
+  const bookCard = document.createElement("div");
+  const bookDescription = document.createElement("p");
+
+  const removeBookButton = document.createElement("button");
+  removeBookButton.classList.add("remove__button");
+
+  const toggleReadButton = document.createElement("button");
+  toggleReadButton.classList.add("toggle__button");
+
+  bookCard.classList.add("book__card");
+  bookCard.append(bookDescription, removeBookButton, toggleReadButton);
+  bookListElement.append(bookCard);
+  return bookCard;
+}
+
+function renderCard(bookCard, book) {
+  const bookDescription = bookCard.querySelector("p");
+  bookDescription.textContent = `Title: ${book.title} Author: ${book.author} Pages: ${book.pages} Read: ${book.read}`;
+  const removeBookButton = bookCard.querySelector(".remove__button");
+  removeBookButton.textContent = "Remove";
+  const toggleReadButton = bookCard.querySelector(".toggle__button");
+  toggleReadButton.textContent = "Read";
+
+  if (book.read === true) {
+    toggleReadButton.textContent = "Unread";
+  } else {
+    toggleReadButton.textContent = "Read";
+  }
+}
+
+function displayLibrary() {
+  const bookListElement = document.getElementById("book-list");
   bookListElement.innerHTML = "";
 
-  myLibrary.forEach((book) => {
-    const paragraph = document.createElement("p");
-    paragraph.textContent = `Title: ${book.title} Author: ${book.author} Pages: ${book.pages} Read: ${book.read}`;
-    bookListElement.appendChild(paragraph);
+  myLibrary.forEach((book, bookIndex) => {
+    const bookCard = createCard(bookListElement);
+
+    renderCard(bookCard, book);
+
+    function handleClick() {
+      myLibrary.splice(bookIndex, 1);
+      bookCard.remove();
+    }
+
+    function handleToggle() {
+      book.read = !book.read;
+      renderCard(bookCard, book);
+    }
+
+    const removeBookButton = bookCard.querySelector(".remove__button");
+    removeBookButton.addEventListener("click", handleClick);
+    const toggleReadButton = bookCard.querySelector(".toggle__button");
+    toggleReadButton.addEventListener("click", handleToggle);
   });
-};
+}
+
+let titleInput = document.getElementById("title");
+let authorInput = document.getElementById("author");
+let pagesInput = document.getElementById("pages");
+let readInput = document.getElementById("read");
+let bookForm = document.getElementById("form");
+
+function clearInputs() {
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  readInput.value = "";
+}
 
 function addBookToLibrary() {
-  let titleInput = document.getElementById("title");
-  let authorInput = document.getElementById("author");
-  let pagesInput = document.getElementById("pages");
-  let readInput = document.getElementById("read");
-  let bookForm = document.getElementById("form");
-
   bookForm.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("Form submitted");
@@ -89,10 +137,7 @@ function addBookToLibrary() {
     // Update the library display
     displayLibrary();
 
-    titleInput.value = "";
-    authorInput.value = "";
-    pagesInput.value = "";
-    readInput.value = "";
+    clearInputs();
   });
 }
 
